db0b0274f5bd2d39a9fa2ef8e7a7784c66f561d5
Scott Chu <skchu@ualberta.ca>
2019-08-13 21:42:53 -0600

    fixing bugs

--- assets/js/streak.js
+++ assets/js/streak.js
@@ -1,35 +1,34 @@
 var Timer = document.querySelector('#Timer');
 var time;
-var isOn = false;
+var toggle = false;
 var ms = 0;
 var s = 0;
 var m = 0;
 
 //Spacebar timer trigger (Getting ready and Stopping timer)
-$("html").keypress(function(){
+$("html").keydown(function() {
   $("#Timer").addClass("ready");
-  if(isOn === true) {
-    if(event.which === 32) {
-      stop();
-      isOn = false;
-    }
-  }
+  stop();
 });
 
 //Spacebar timer trigger (Starting timer)
 $("html").keyup(function(event) {
-  if(isOn === false) {
-    if(event.which === 32) {
+  if (toggle === false) {
+    if (event.which === 32) {
       $("#Timer").removeClass("ready");
-      isOn = true;
+      toggle = true;
       start();
     }
   }
 });
 
 
+
+
+
+
 // ----------------------------------------------------------------------------//
-//Stopwatch functionality
+//Stopwatch functionalities
 function start() {
   if(!time) {
     time = setInterval(run, 10);
@@ -40,7 +39,7 @@ function run() {
   if(m == 0 ) {
     Timer.textContent = s + "." + ms;
   } else {
-    Timer.textContent = m + "." + s + "." + ms;
+    Timer.textContent = m + "." + (s < 10 ? "0" + s : s) + "." + ms;
   }
 
   ms++;
@@ -57,5 +56,4 @@ function run() {
 function stop() {
   clearInterval(time);
   time = false;
-  $("#Timer").css("color", "black");
 }
